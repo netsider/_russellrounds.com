ba5e460bb9758de04b6e9b91d67108af03400830
Russell Rounds <rdoubleoc@aol.com>
2016-07-17 04:57:51 +0000

    First progress
    first real progress

--- index.php
+++ index.php
@@ -1,55 +1,57 @@
 <html><head>
 <title>It worked!</title>
 <meta charset="utf-8">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head><body>
 <style>
-.col-sm-12{
-	margin: auto;
-	text-align: center;
-}
-.row{
+body{
+	background: #000000;
 }
 #box{
 	border-style: solid;
-	border-width: 1px 1px 1px 1px;
+	border-width: 0px;
+	width: 60%;
+	height: 75%;
+	border-radius: 25px;
+	background: #FFFFFF;
+	box-shadow: 0px 0px 1px #000000;
+}
+.border{
+	border-style: solid;
+	border-width: 1px;
+}
+.innerbox{
+	border-radius: 25px 25px 0px 0px;
+	text-align: center;
 }
 </style>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Russellrounds.com</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span>A</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>B</a></li>
-    </ul>
+  <div id="box">
+  
+  <div class="innerbox border">
+  <h2>Welcome to RussellRounds.com</h2>
+    <div class="row">
+  <div class="col-sm-4 border">.col-sm-4</div>
+  <div class="col-sm-4  border">.col-sm-4</div>
+  <div class="col-sm-4  border">.col-sm-4</div>
+</div>
+  
+  
+  </div>  
   </div>
-</nav>
+  
 
-<div class="row">
-  <div class="col-sm-12"><h2>Russellrounds.com</h2></div>
-</div>
-<div class="row">
-  <div class="col-sm-4" id="box">A</div>
-  <div class="col-sm-4" id="box">A</div>
-  <div class="col-sm-4" id="box">A</div>
-</div>
-<div class="row">
-  <div class="col-sm-4" id="box">B</div>
-  <div class="col-sm-4" id="box">B</div>
-  <div class="col-sm-4" id="box">B</div>
-</div>
-<div class="row">
-  <div class="col-sm-4" id="box">C</div>
-  <div class="col-sm-4" id="box">C</div>
-  <div class="col-sm-4" id="box">C</div>
-</div>
 
+ <script>
+$(window).resize(function(){
+	$('#box').css({
+		position:'fixed',
+		left: ($(window).width() - $('#box').outerWidth())/2,
+		top: ($(window).height() - $('#box').outerHeight())/2
+	});
+});
+
+$(window).resize();
+</script>
 </body></html>
