c7422c0859d27a6157a152b770a522d47889073a
Russell Rounds <rdoubleoc@aol.com>
2016-07-17 01:44:05 +0000

    Long over due
    First update in a long time

--- index.php
+++ index.php
@@ -1,7 +1,55 @@
 <html><head>
 <title>It worked!</title>
-
+<meta charset="utf-8">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head><body>
-<h1>It Worked!</h1>
-<h2>russellrounds.com</h2>
+<style>
+.col-sm-12{
+	margin: auto;
+	text-align: center;
+}
+.row{
+}
+#box{
+	border-style: solid;
+	border-width: 1px 1px 1px 1px;
+}
+</style>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Russellrounds.com</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span>A</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>B</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div class="row">
+  <div class="col-sm-12"><h2>Russellrounds.com</h2></div>
+</div>
+<div class="row">
+  <div class="col-sm-4" id="box">A</div>
+  <div class="col-sm-4" id="box">A</div>
+  <div class="col-sm-4" id="box">A</div>
+</div>
+<div class="row">
+  <div class="col-sm-4" id="box">B</div>
+  <div class="col-sm-4" id="box">B</div>
+  <div class="col-sm-4" id="box">B</div>
+</div>
+<div class="row">
+  <div class="col-sm-4" id="box">C</div>
+  <div class="col-sm-4" id="box">C</div>
+  <div class="col-sm-4" id="box">C</div>
+</div>
+
 </body></html>
