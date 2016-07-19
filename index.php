c49f0c7312783eb191a9cbd6efb900f95e2c6b86
Russell Rounds <rdoubleoc@aol.com>
2016-07-19 23:55:36 +0000

    11:55 7/19/16
    Title change.  First commit for tonight.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html><head>
-<title>It worked!</title>
+<title>RussellRounds.com</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
