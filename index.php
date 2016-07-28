2328db479bee963df09f5cab05f47ebf57d6152a
Russell Rounds <rdoubleoc@aol.com>
2016-07-28 23:18:56 +0000

    7/28/16 11:18pm
    Misc. changes.

--- index.php
+++ index.php
@@ -8,16 +8,22 @@
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head><body onload="$(window).resize();">
 <style>
-@media (max-width: 4000px) {
+@media (min-width: 1000px) {
   #mainBox {
     width: 40%;
 	height: 80%;
+  }
+  #myphoto{
+	  width:45%;
   }
 }
 @media (max-width: 1000px) {
   #mainBox {
     width: 90%;
 	height: 80%;
+  }
+    #myphoto{
+	  width:60%;
   }
 }
 body{
@@ -61,49 +67,54 @@ body{
 	text-align: center;
 }
 #myphoto{
-	width:60%;
 	height:auto;
 	position: relative;
 	top: 10%;
-	box-shadow: 1px 1px 6px 1px #000000;
+	box-shadow: 1px 1px 5px 1px #000000;
 }
 #secondColumn{
 	width:35%;
 	float:left;
-	height:40%;
+	height:30%;
 	line-height: 200%;
 }
 #firstColumn{
 	width: 65%;
 	float:left;
-	height: 40%;
+	height: 30%;
 	overflow: hidden;
 }
 #mainRow{
 	width:100%;
 	float:left;
-	height:50%;
+	height:60%;
 	text-align: center;
 	padding: 2px;
 }
 .mainText{
 	position: relative;
 }
+hr{
+	border-top: 1px solid #054a7b;
+}
 </style>
   <div id="mainBox" class="border">
   
 	<div id="titleBar">&nbsp</div>  
-	<div id="firstColumn"><div><img src="russsuit.jpg" id="myphoto" class="center img-rounded"></div></div><div id="secondColumn"><div id="menu" class="">Menu:<hr width="75%" size="3px" style="margin-top:0;margin-bottom:0;"/><a href="#" onclick="showHome()">Home</a><br/><a href="#" onclick="showContact()">Contact</a><br/><a href="#" onclick="showLinks()">Links</a><br/><br/>
+	<div id="firstColumn"><div><img src="russsuit.jpg" id="myphoto" class="center img-rounded"></div></div><div id="secondColumn"><div id="menu" class="">Menu<hr width="75%" size="3px" style="margin-top:0;margin-bottom:0;margin-top:-5px;"/><a href="#" onclick="showHome()">Home</a><br/><a href="#" onclick="showContact()">Contact</a><br/><a href="#" onclick="showLinks()">Links</a><br/><br/>
 	
 	
 	</div>
 	
 	</div><br/>
 	<div id="mainRow">
-	<hr width="75%" size="3px" />
-	<p class="mainText" id="homeText">Welcome to my website.  Please use the links above to navigate content.</p>
-	<p class="mainText" id="contactText">Contact me:<br/><br/><strong>Email:</strong> <a href="mailto:netside@protonmail.com">netside@protonmail.com</a><br/><strong>Facebook:</strong> <a href="http://facebook.com/russellrounds">http://facebook.com/russellrounds</a></p>
-	<p class="mainText" id="linkText">Links:<br/><br/><strong>Github:</strong> <a href="https://github.com/netsider">https://github.com/netsider</a></p></div>
+	<hr width="75%" size="3px" style="margin-bottom:0px;"/>
+	<p><div class="mainText" id="homeText">Welcome to my website.  Please use the links above to navigate content.</p></div>
+	<p><div class="mainText" id="contactText">Contact me:<br/><strong>Email:</strong> <a href="mailto:netside@protonmail.com">netside@protonmail.com</a><br/><strong>Facebook:</strong> <a href="http://facebook.com/russellrounds">http://facebook.com/russellrounds</a></p></div>
+	<div class="mainText" id="linkText"><p>Links:<br/><strong>Github:</strong> <a href="https://github.com/netsider">https://github.com/netsider</a></p></div>
+	
+	
+	</div>
 	
 	<br/><div id="footer">Created by <a href="mailto:russellrounds@gmail.com">Russell Rounds</a>. &copy; 2016</div>  
   </div>
